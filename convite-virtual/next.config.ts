c57837789eb84d54117438ad5cae9ea3be3029b0
Ryan Monteiro <monteroryang@gmail.com>
2026-03-02 14:58:45 -0300

    Update next.config.ts

--- convite-virtual/next.config.ts
+++ convite-virtual/next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Substitua 'anac' pelo nome exato do seu repositório no GitHub
+  basePath: '/anac',
+  assetPrefix: '/anac',
+  output: 'export', // Necessário para o GitHub Pages
+  images: {
+    unoptimized: true, // Necessário para exportação estática
+  },
 };
 
 export default nextConfig;
