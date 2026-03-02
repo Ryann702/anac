11b9913055edf09eca7453c28190afb06c1d2a50
Ryan Monteiro <monteroryang@gmail.com>
2026-03-02 18:17:51 +0000

    final

--- convite-virtual/next.config.ts
+++ convite-virtual/next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Substitua 'anac' pelo nome exato do seu repositório no GitHub
-  basePath: '/anac',
-  assetPrefix: '/anac',
-  output: 'export', // Necessário para o GitHub Pages
   images: {
-    unoptimized: true, // Necessário para exportação estática
+    unoptimized: true,
+  },
   },
 };
 
